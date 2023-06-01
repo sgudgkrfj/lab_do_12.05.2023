813c0f5baa27afa8b99df340c64710230c9b0ed7
sgudgkrfj <131796501+sgudgkrfj@users.noreply.github.com>
2023-06-01 08:34:16 +0300

    #2

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
-import time
+def cache(func):
+    memo = {}
 
-def gggggg(func):
-    def wrapper(n):
-        start_time=time.time()
-        for i in range(n):
-            print("ddd")
-        end_time=time.time()
-        tiime=end_time-start_time
-        print(tiime)
+    def wrapper(*args, **kwargs):
+        key = (args, tuple(kwargs.items()))
+        if key in memo:
+            return memo[key]
+        else:
+            result = func(*args, **kwargs)
+            memo[key] = result
+            return result
+
     return wrapper
-@gggggg
-def ddd(n):
-    n=n
-    return n
-ddd(520)
